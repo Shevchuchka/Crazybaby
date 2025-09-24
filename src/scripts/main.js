f6f33418674862705ae7d3d9821be98ddabb0d3e
Yuliia <shevchuuchkaaa@gmail.com>
2025-09-24 21:33:04 +0200

    fetchpriority

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,19 +6,22 @@ const {
   toggleLangFunc,
   setENBtn,
   setUABtn,
-} = require('../scripts/modules/toggleLang.js');
+} = require("../scripts/modules/toggleLang.js");
 
-const { pageElement, checkNumFunc } = require('./modules/featuresSwiperPagination.js');
+const {
+  pageElement,
+  checkNumFunc,
+} = require("./modules/featuresSwiperPagination.js");
 
 const { prevBtn, nextBtn, activeArticle } = pageElement;
 const { checkNum } = checkNumFunc;
 
-const { elements, functions } = require('./modules/forms.js');
+const { elements, functions } = require("./modules/forms.js");
 
 const { form, email, message, sendBtn } = elements;
 const { onBlur, onFocus, onInput, onSubmit, resetErrors } = functions;
 
-new Swiper('.swiper__benefits', {
+new Swiper(".swiper__benefits", {
   loop: true,
 
   autoplay: {
@@ -27,17 +30,17 @@ new Swiper('.swiper__benefits', {
   },
 });
 
-new Swiper('.swiper__features', {
+new Swiper(".swiper__features", {
   navigation: {
-    nextEl: '.toggles__toggle--next',
-    prevEl: '.toggles__toggle--prev',
+    nextEl: ".toggles__toggle--next",
+    prevEl: ".toggles__toggle--prev",
   },
 });
 
 const totalArticles = 3;
 let articleNum = 1;
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
   if (articleNum === 1) {
     return;
   }
@@ -48,7 +51,7 @@ prevBtn.addEventListener('click', () => {
   checkNum(articleNum, totalArticles);
 });
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener("click", () => {
   if (articleNum === totalArticles) {
     return;
   }
@@ -60,26 +63,26 @@ nextBtn.addEventListener('click', () => {
 });
 
 setENBtn.forEach((el) => {
-  el.addEventListener('click', () => {
-    el.classList.remove('inactive');
+  el.addEventListener("click", () => {
+    el.classList.remove("inactive");
 
-    toggleLangFunc('en');
+    toggleLangFunc("en");
   });
 });
 
 setUABtn.forEach((el) => {
-  el.addEventListener('click', () => {
-    el.classList.remove('inactive');
+  el.addEventListener("click", () => {
+    el.classList.remove("inactive");
 
-    toggleLangFunc('ua');
+    toggleLangFunc("ua");
   });
 });
 
-email.form.addEventListener('click', () => {
+email.form.addEventListener("click", () => {
   onFocus(email, message);
 });
 
-email.input.addEventListener('input', (e) => {
+email.input.addEventListener("input", (e) => {
   resetErrors(email);
 
   setTimeout(() => {
@@ -89,16 +92,16 @@ email.input.addEventListener('input', (e) => {
   }, 500);
 });
 
-email.input.addEventListener('blur', () => {
+email.input.addEventListener("blur", () => {
   onBlur(email);
   onBlur(message);
 });
 
-message.form.addEventListener('click', () => {
+message.form.addEventListener("click", () => {
   onFocus(message, email);
 });
 
-message.input.addEventListener('input', (e) => {
+message.input.addEventListener("input", (e) => {
   resetErrors(message);
 
   setTimeout(() => {
@@ -108,18 +111,18 @@ message.input.addEventListener('input', (e) => {
   }, 500);
 });
 
-message.input.addEventListener('blur', () => {
+message.input.addEventListener("blur", () => {
   onBlur(email);
   onBlur(message);
 });
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   onSubmit(email.input, message.input);
 });
 
-sendBtn.addEventListener('click', (e) => {
+sendBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   onSubmit(email.input, message.input);
